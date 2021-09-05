24f047b00884c3216c4bcaa3374ff02a7798e077
Okrul <okrul@list.ru>
2021-09-05 15:24:38 +0300

    feat: imlement guessing game

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,26 @@
 class GuessingGame {
     constructor() {}
 
-    setRange(min, max) {
+    minValue = 0;
+    maxValue = 0;
+    num = 0;
 
+    setRange(min, max) {
+        this.minValue = min;
+        this.maxValue = max;
     }
 
     guess() {
-
+        this.num = this.minValue + Math.round((this.maxValue - this.minValue)/2);
+        return this.num; 
     }
 
     lower() {
-
+        this.maxValue =  this.num;  
     }
 
     greater() {
-
+        this.minValue = this.num;
     }
 }
 
